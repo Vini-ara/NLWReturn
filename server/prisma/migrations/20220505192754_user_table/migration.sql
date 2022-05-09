8bf2a8b3656dbd225bb327baf0358445dc58a1ac
Vinicius Araujo <vinicius.ar02@gmail.com>
2022-05-08 23:56:48 -0300

    removed migrations

--- server/prisma/migrations/20220505192754_user_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "admin" BOOLEAN NOT NULL,
-    "Password" TEXT NOT NULL
-);
